70472599e62b3a5e59aff3e48a50ec15098b2425
divin-d1 <rukundof993@gmail.com>
2026-02-16 12:00:00 +0000

    [Update]: Integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavbarWrapper from "./components/NavbarWrapper";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +37,7 @@ export default function RootLayout({
       >
         <NavbarWrapper />
         {children}
+        <ToastContainer position="bottom-right" theme="dark" />
       </body>
     </html>
   );
